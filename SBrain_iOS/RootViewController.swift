1710e7f942f2e14113db62e962c06310a7637e62
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-02 15:14:04 -0300

    Add initial root view controller

---
+++ SBrain_iOS/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchViewController.swift
+//  SBrain_iOS
+//
+//  Created by Vitor S do Nascimento on 02-11-19.
+//  Copyright © 2019 Second Brain Project. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    var exampleTest = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        exampleTest = true
+    }
+}
