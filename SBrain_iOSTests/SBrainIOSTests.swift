3b0192ee645029e2351d16f2e074db384aa11509
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-02 00:10:47 -0300

    Add initial target SBrain_iOS and SBrain_iOSTests

---
+++ SBrain_iOSTests/SBrainIOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  SBrainIOSTests.swift
+//  SBrain_iOSTests
+//
+//  Created by Vitor S do Nascimento on 01-11-19.
+//  Copyright © 2019 Second Brain Project. All rights reserved.
+//
+
+import XCTest
+@testable import SBrain_iOS
+
+class SBrainIOSTests: XCTestCase {
+
+    func testExample() {
+
+    }
+
+}
